933a50860e9c29ba074b54fe0237fd9f0b2e4e99
unrealinux <121058689@qq.com>
2017-05-04 22:30:38 +0800

    Initial Commit

---
+++ DataProcessPlatformIOSSDK/DataProcessPlatformIOSSDK.h
@@ -0,0 +1,19 @@
+//
+//  DataProcessPlatformIOSSDK.h
+//  DataProcessPlatformIOSSDK
+//
+//  Created by xielinus on 2017/5/4.
+//  Copyright © 2017年 circletechcircletech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataProcessPlatformIOSSDK.
+FOUNDATION_EXPORT double DataProcessPlatformIOSSDKVersionNumber;
+
+//! Project version string for DataProcessPlatformIOSSDK.
+FOUNDATION_EXPORT const unsigned char DataProcessPlatformIOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataProcessPlatformIOSSDK/PublicHeader.h>
+
+
